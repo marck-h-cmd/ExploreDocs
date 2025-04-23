198a6b2a4057eca95d5dc593f98bb8fdb31a22d0
Pandaman <147335739+Pandaman08@users.noreply.github.com>
2025-04-23 00:43:53 -0500

    ania

--- resources/views/layout/user/template.blade.php
+++ resources/views/layout/user/template.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <!-- Splide CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <!--<link href="https://fonts.googleapis.com/css?family=Glass+Antiqua" rel="stylesheet">
@@ -20,55 +22,55 @@
 </head>
 
 <body>
-      <!-- Nav -->
+    <!-- Nav -->
     <nav class="navbar navbar-expand-lg fixed-top bg-light" id="eenavbar">
         <div class="container ">
-          <!-- Logo -->
-          <a class="navbar-brand" href="#">
-            <img src="eelogo5.png" alt="Logoimg" class="img-fluid Logoimg" height="40">
-          </a>
-      
-          <!-- Hamburger Button -->
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-      
-          <!-- Collapsible Navigation -->
-          <div class="collapse navbar-collapse eenavbar" id="myNavbar">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
-              </li>
-      
-              <!-- Browse Dropdown -->
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="browseDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Browse
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="browseDropdown">
-                  <li><a class="dropdown-item" href="ee-landmarks-en.html">Landmarks</a></li>
-                  <li><a class="dropdown-item" href="ee-spirtual-en.html">Spiritual</a></li>
+            <!-- Logo -->
+            <a class="navbar-brand" href="#">
+                <img src="eelogo5.png" alt="Logoimg" class="img-fluid Logoimg" height="40">
+            </a>
+
+            <!-- Hamburger Button -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Collapsible Navigation -->
+            <div class="collapse navbar-collapse eenavbar" id="myNavbar">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+
+                    <!-- Browse Dropdown -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="browseDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Browse
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="browseDropdown">
+                            <li><a class="dropdown-item" href="ee-landmarks-en.html">Landmarks</a></li>
+                            <li><a class="dropdown-item" href="ee-spirtual-en.html">Spiritual</a></li>
+                        </ul>
+                    </li>
+
+                    <!-- Explore Dropdown -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="exploreDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Explore
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="exploreDropdown">
+                            <li><a class="dropdown-item" href="#">Adventure</a></li>
+                            <li><a class="dropdown-item" href="#">Sport</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
                 </ul>
-              </li>
-      
-              <!-- Explore Dropdown -->
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="exploreDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Explore
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="exploreDropdown">
-                  <li><a class="dropdown-item" href="#">Adventure</a></li>
-                  <li><a class="dropdown-item" href="#">Sport</a></li>
-                </ul>
-              </li>
-      
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
-              </li>
-            </ul>
-          </div>
+            </div>
         </div>
-      </nav>
+    </nav>
 
 
 
@@ -104,8 +106,10 @@
         <h4>© 2025 Group-21.ICS-BUE, All rights reserved.</h4>
     </footer>
 
-   
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Splide JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js"></script>
 
 
 
